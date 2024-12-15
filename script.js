0ba0272c0e7343b8159d774b018f2457b2f38cde
samiatt <138974205+samiatt@users.noreply.github.com>
2024-12-15 04:14:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set language based on selection and store in a cookie
 function setLanguage(language) {
-    document.cookie = `language=${language};path=/;max-age=31536000`; // Set cookie for 1 year
+    document.cookie = language=${language};path=/;max-age=31536000; // Set cookie for 1 year
     loadLanguage(language);
 }
 
